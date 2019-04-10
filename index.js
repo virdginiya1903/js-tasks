a59153854021692df24f13becbd142818489dbf5
virdginiya1903 <virdginiya1903@mail.ru>
2019-04-10 19:58:12 +0300

    решена задача: изменение символов в строке

--- index.js
+++ index.js
@@ -245,10 +245,29 @@ function repeatStr(n, s) {
 repeatStr(2, "ha ");
 console.log(repeatStr(2, "ha "));
 
+// Given the string representations of two integers, return the string representation of the sum of those integers.
 function sumStrings(a, b) {
-  let resSumStrings = Number(a) + Number(b);
+  let resSumStrings = (Number(a) + Number(b)).toFixed(0);
   return String(resSumStrings);
 }
-console.log(sumStrings("7125", "810037"));
 
-sumStrings("7125", "810037");
+// Character recognition software is widely used to digitise printed texts. Thus the texts can be edited, searched and stored on a computer.
+
+// When documents (especially pretty old ones written with a typewriter), are digitised character recognition softwares often make mistakes.
+
+// Your task is correct the errors in the digitised text. You only have to handle the following mistakes:
+
+// S is misinterpreted as 5
+// O is misinterpreted as 0
+// I is misinterpreted as 1
+// The test cases contain numbers only by mistake.
+
+function correct(string) {
+  // your code here
+  return string
+    .replace(/5/g, "S")
+    .replace(/0/g, "O")
+    .replace(/1/g, "I");
+}
+correct("PAR15");
+correct("51NGAP0RE");
