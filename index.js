1f1ede8a96fe7eec4e54c5d1f14471cd019c4310
Guzel Akhmetova <virdginiya1903@mail.ru>
2020-01-19 13:51:48 +0300

    задача первый и последний символ строки

--- index.js
+++ index.js
@@ -317,3 +317,16 @@ function invert(array) {
 }
 
 invert([1, -2, 3, -4, 5]);
+
+
+// Выведите на экран первую и последнюю букву предложения, записанные в константу text, в следующем формате:
+
+// First: N
+// Last: t
+// Ваша задача извлечь эти символы из строки и вставить в console.log, не используя промежуточные переменные.
+const text = 'Never forget what you are, for surely the world will not';
+
+// BEGIN
+console.log(`First: ${text[0]}
+Last: ${text[text.length-1]}`)
+// END
